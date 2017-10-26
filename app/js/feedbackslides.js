f05ca42891612c1dd9c8ba1b52f5df70d1bae28b
xalkovich <xalkovich@gmail.com>
2017-10-26 18:14:46 +0300

    media

--- app/js/feedbackslides.js
+++ app/js/feedbackslides.js
@@ -1,14 +1,22 @@
-$(document).ready(function(){
-// Slider
+$(document).ready(function () {
+  // Slider
   $('.feedbackslides').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  // autoplay: true,
-  autoplaySpeed: 5000,
-  useTransform:false,
-  pauseOnHover: false,
-  // adaptiveHeight: true
-  // speed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 5000,
+    useTransform: false,
+    pauseOnHover: false,
+    // adaptiveHeight: true
+    // speed: 2000,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },]
   });
 
 });
