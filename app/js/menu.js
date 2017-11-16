d1585d3acbb1f294c775656324f7e100046a7c84
xalkovich <xalkovich@gmail.com>
2017-11-16 13:52:03 +0200

    work 16.11

--- app/js/menu.js
+++ app/js/menu.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.line').click(function () {
         $('.line').hide('fade', 'fast', function () {
             $('.line').addClass('hidden', function () {
+                $('.back').css('opacity','0.7');
                 $('.navSmall').show('slide', 'slow').addClass('visible');
                 $('.about').css({ 'paddingLeft': '24vh', 'transition': '1000ms' });
                 $('.feedback').css({ 'paddingLeft': '20vh', 'transition': '1000ms' });
@@ -13,6 +14,7 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('.closeTop, .closeBottom').click(function () {
+        $('.back').css('opacity', '0');
         $('.about').css({ 'paddingLeft': '0vh' });
         $('.feedback').css({ 'paddingLeft': '0vh' });
         $('.line').show('fade', 500).removeClass('hidden');
